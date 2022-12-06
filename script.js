ea61c46b3fbe55960a3abd84db61fd0b2815db3c
MuhammadAlfariqo <riquchan6@gmail.com>
2022-12-06 17:29:54 +0700

    menambah fungsi hitung

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  let tempTotalHarga = 0;
 
-	$('input#totalBelanja').keyup(function(){
-		let totalBelanja = $('input#totalBelanja').val();
-		let diskon = 0;
-		if(totalBelanja >= 100000){
-			diskon = 10;
-		} else if(totalBelanja >= 50000){
-			diskon = 5;
-		}
+  $("input#totalBelanja").keyup(function () {
+    let totalBelanja = $(this).val();
+    let diskon = 0;
+    if (totalBelanja >= 200000) {
+      diskon = 5;
+    } else if (totalBelanja >= 150000) {
+      diskon = 3;
+    }
+    let totalHarga = totalBelanja - (diskon / 100) * totalBelanja;
+    tempTotalHarga = totalHarga;
+    $("input#diskon").val(diskon);
+    $("input#totalHarga").val(totalHarga.toLocaleString("id-ID"));
+  });
 
-		let totalHarga = $(this).val() - ((diskon/100)* $(this).val());
-
-		$('td#diskon').text(`${diskon} %`);
-		$('td#totalHarga').text(totalHarga);
-	});
-
-	$('input#tunai').keyup(function(){
-		let kembalian = $(this).val() - $('td#totalHarga').text();
-
-		$('td#kembalian').text(kembalian);
-	});
-});	
+  $("input#tunai").keyup(function () {
+    let kembalian = $(this).val() - tempTotalHarga;
+    $("input#kembalian").val(kembalian.toLocaleString("id-ID"));
+  });
+});
